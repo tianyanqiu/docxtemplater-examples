2eb17061171d493d62e5ee250e969e8e4c72f8f1
tianyanqiu <745916729@qq.com>
2019-11-28 14:46:11 +0800

    chore: 添加子表单渲染列表

--- src/react-table-example.ts
+++ src/react-table-example.ts
@@ -1,8 +1,13 @@
 import genDocx from './genDocx';
 import content from './react-table-content';
+import SubformList from './SubformList';
+import data from './data';
 
 genDocx('basic-style-template.docx', 'react-table.docx', {
   内容: {
     react: content,
+  },
+  子表单: {
+    react: SubformList({ data }),
   },
 });
